3d5789d5ea19e5e78ad2c422dc36c0b17d530326
Adaso <adamsdm1@gmail.com>
2017-01-31 13:09:13 +0100

    fixed eDistance

--- lab2/js/kmeans.js
+++ lab2/js/kmeans.js
@@ -8,16 +8,16 @@
     function kmeans(data, k) {
         var clusters = [];
 
+
+        const DIMS = d3.keys(data[0]).length;
+
         // Position cluster centroids
         for(var i=0; i<k; i++){
-            clusters[i] = {
-                "Ac": Math.random(),
-                "Bc": Math.random(),
-                "Dc": Math.random()
-            };
+            clusters[i] = [];
+            for(var dim=0; dim<DIMS; dim++){
+                clusters[i][dim] = Math.random();
+            }
         }
-
-        console.log(clusters);
 
         // while error < Threshold
         // if max iterations => early termination
@@ -25,19 +25,32 @@
             data.forEach(function(d, i){
                 // Calculate distance to each cluster point
                 // Assign distance to closest cluster
-
                 if(d.A<0.5) {
                     d.cluster = 0;
                 } else {
                     d.cluster = 1;
                 }
             });
+
+            // Update cluster centroids
         // endWhile
+    //return data
 
     };
 
+    var a = [1,2,3];
+    var b = [1,2,3];
+
+    console.log(eDistance(a,b));
+
     // calculate N-dimensional euclidian distance from d1 to d2
-    function eDistance(d1, d2){
-        // rootSum = ...
-        // return sqrt(rootSum);
+    function eDistance(c, l) {
+        var rootSum = 0;
+
+        // Calculate root sum
+        for(var i=0; i<c.length; i++){
+            rootSum+= Math.pow(c[i]-l[i], 2);
+        }
+
+        return Math.sqrt(rootSum);
     }
