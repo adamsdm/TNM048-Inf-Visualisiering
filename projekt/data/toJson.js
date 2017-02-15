8abd3251a5f514e57c118f249faf76e7d246d5b6
oscar komulainen <oscar komulainen>
2017-02-15 16:02:47 +0100

    fixed bug with weather signs in the coordinate format

--- projekt/data/toJson.js
+++ projekt/data/toJson.js
@@ -49,7 +49,7 @@ fs.readFile(input, 'utf8', function (err,contents) {
        
 
         // Only push if object has testing party or coordinate
-        if(obj.testingParty    || ( coord[0] && coord[1] ) )
+        if(obj.testingParty    && ( coord[0] && coord[1] ) )
             jsonData.detonations.push(obj);
     }
 
@@ -96,18 +96,15 @@ function formatCoord(lat, lon, site){
         // If 'S' or 'W' is found in string
         // Remove and replace with '-'
         if(sInd != -1 ){
-            newLat = lat.slice(0, lat.length-1); // Remove 'S'
             newLat = "-" + newLat;  // Add '-'
         }
         if(wInd != -1){
-            newLon = lon.slice(0, lon.length-1); // Remove 'S'
             newLon = "-" + newLon;  // Add '-'
         }
 
-        // Remove 'E'and 'N'
-        if(nInd != -1 ) newLat = lat.slice(0, lat.length-1); // Remove 'N'
-        if(eInd != -1 ) newLon = lon.slice(0, lon.length-1); // Remove 'E'
 
+        newLat = newLat.replace(/[^\d.-]/g, '');
+        newLon = newLon.replace(/[^\d.-]/g, '');
 
         var coord = [newLon, newLat];
         return coord;
